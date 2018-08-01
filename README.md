ebc3ceec2dce6b79f7df521111d5c6f54d62579d
Mace Ojala <mace@kirjakaapeli.lib.hel.fi>
2018-08-01 15:37:43 +0200

    Committing a working version, plus updated the README for usage instructions

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # deletePodioSpamEmailComments
+
 A JavaScript cleaning routine for cleaning Podio spam comments sent via the Email to item feature
+
+Super annoying that [Podio](https://podio.com) has weak or nonexisting spam protection, and does not allow disabling this feature per user or altogether for a workspace.
+
+## Usage
+
+This is used by logging in to Podio as the user, while setting `spamUid`, and copy+pasting the whole lot to browser console, and then waiting.
---
+++ deletePodioSpamEmailComments.js
@@ -0,0 +1,48 @@
+// Add the user id to remove in here. There is a good chance it is
+// a six-digit number
+var spamUid = '550819';
+
+// Consider only comments made after this date
+var spamDate = new Date(2018, 04, 01);
+
+var spamUserUrl = 'https://podio.com/users/' + spamUid;
+
+var mnum = {
+    'January': 0,
+    'February': 1,
+    'March': 2,
+    'April': 3,
+    'May': 4,
+    'June': 5,
+    'July': 6,
+    'August': 7,
+    'September': 8,
+    'October': 9,
+    'November': 10,
+    'December': 11
+}
+
+var titleToDate = (title) => {
+    re = /(\s*\d*) ([A-Za-z]*) (\d{4}) (\d*):(\d*)/;
+    [, D, M, Y, h, m] = re.exec(title);
+    d = new Date(Y, mnum[M], D, h, m);
+    return d;
+}
+
+var throttling = 2000;
+
+var spamComments = jQuery('.comment')
+    .filter((i, c) => jQuery(c).find('.comment_byline > a')[0].href == spamUserUrl)
+    .filter((i, c) => jQuery(c).find('.timestamp > time')[0].title != "")
+    .filter((i, c) => titleToDate(jQuery(c).find('.timestamp > time')[0].title) > spamDate);
+
+console.log("Found " + spamComments.length + " comments to delete");
+
+spamComments.each((i, c) => {
+    c.style.border = '5px red dotted';
+    setTimeout(() => {
+	console.log("deleting 💩", c);
+	jQuery(c).find('.js-delete-comment').click();
+	jQuery('.confirm-button')[0].click();
+    }, i * throttling);
+});
